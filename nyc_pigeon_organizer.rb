c336f6de3301b27b19943cbcd5a505b9e84bec24
Chhaian Pin <chhaianpin@hotmail.com>
2019-01-22 21:50:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  h1 = {}
+  data.each {|first_key, first_values|
+    first_values.each {|second_key, second_values|
+      h2 = {}
+      second_values.each {|name|
+        if !h1.has_key?(name)
+          h2 = {name => first_key}
+          if second_values.include?(name)
+            h2[name] = {first_key => [second_key.to_s]}
+        end
+        elsif second_values.include?(name)
+          if h1[name].has_key?(first_key)
+            h1[name][first_key] << second_key.to_s
+          else
+            h2 = {first_key => [second_key.to_s]}
+            h2 = h1[name].merge(h2)
+            h2 = {name => h2}
+          end    
+        end
+        h1 = h1.merge(h2)
+      }
+    }
+  }
+  h1
 end
