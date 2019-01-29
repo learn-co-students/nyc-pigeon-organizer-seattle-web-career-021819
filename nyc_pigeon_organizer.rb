bb06f05cea42e99ecd90edea0e080988632931f0
Joshua M Loran <joshua.loran@gmail.com>
2019-01-29 17:33:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_list = {}
+  data.each do |list, hashes|
+    hashes.each do |attribute, array|
+      array.each do |name|
+        if new_list.has_key?(name)
+          if new_list[name].has_key?(list)
+            new_list[name][list] << attribute.to_s
+          else
+            new_list[name] = new_list[name].merge({list => [attribute.to_s]})
+          end
+        else
+          new_list[name] = {list => [attribute.to_s]}
+        end
+      end
+    end
+  end
+  new_list
 end
